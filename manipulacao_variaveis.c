bbd05fde8ffd9e0078a59c04cb4981d0d5ce745f
CastroCamila12 <camiladecastro18@yahoo.com.br>
2025-05-10 21:45:16 +0000

    primeira edição

---
+++ manipulacao_variaveis.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+/*  float x =5.5;
+    float y = 2.2;
+    float soma = x + y;
+    float diferenca = x - y;
+    float produto = x * y;
+    float quociente = x / y;
+
+    printf("soma: %.2f\n", soma);
+    printf("diferença: %.2f\n", diferenca);
+    printf("produto: %.2f\n", produto);
+    printf("quociente: %.2f\n", quociente);*/
+
+    int a = 1;
+    int b = 2;
+    float quociente = (float) a / b;
+
+    printf("quociente: %.2f\n", quociente);
+
+
+
+
+
+
+return 0;
+
+
+}
